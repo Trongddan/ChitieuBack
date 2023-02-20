1240458287d998c8de25d7139bcd957cdfa20559
Trongddan <doandan301@gmail.com>
2023-02-20 22:47:04 +0700

    pk

--- src/app/model/feeType.js
+++ src/app/model/feeType.js
@@ -1,5 +1,10 @@
-const mongoose = require('mongoose');
-const FeeType =new mongoose.Schema({
-    name:{type:String,required:true,unique:true}
-},{timestamps:true})
-module.exports = mongoose.model("feetype",FeeType)
+const mongoose = require("mongoose");
+const FeeType = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    user: { type: mongoose.Types.ObjectId, ref: "users"},
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("feetype", FeeType);
+yield
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const userRoute = require("./userRoute");
+const feeRouter = require("./feeRoute")
 const Route=(app)=> {
   app.use("/user", userRoute);
+  app.use("/fee",feeRouter)
 }
 module.exports = Route;
