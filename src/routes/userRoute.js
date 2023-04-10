f2d96f285344a3423efec179e7c14a7139816d0c
Trongddan <doandan301@gmail.com>
2023-04-10 16:32:24 +0700

    ok

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,6 +3,6 @@ const middlaware = require("../app/middlaware/middlaware")
 const UserController = require("../app/controllers/UserController");
 Router.post("/register", UserController.register);
 Router.post("/login", UserController.login);
-Router.get("/getuser/:id",middlaware.verifyTokenUser,UserController.getUserByID)
+Router.get("/getuser/:id",UserController.getUserByID)
 Router.put("/add-coin/:id", UserController.addCoin);
 module.exports = Router;
